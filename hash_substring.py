f9d3f1b3e9c417cc3bced10253be1a553e96154f
221RDB153 <127057841+221RDB153@users.noreply.github.com>
2023-04-17 01:47:51 +0000

    v3

--- hash_substring.py
+++ hash_substring.py
@@ -1,30 +1,30 @@
 # python3
 
 def read_input():
-    input_str = input().rstrip()
-    input_choice, pattern, text = input_str.split()
-    if input_choice.upper() == 'F':
-        with open(pattern) as f:
-            pattern = f.readline().rstrip()
-            text = f.readline().rstrip()
+    input_type = input()
+    if 'I' in input_type:
+        pattern = input().rstrip()
+        text = input().rstrip()
+    elif 'F' in input_type:
+        filename = "06"
+        with open("tests/" + filename, 'r') as file:
+            pattern = file.readline().rstrip()
+            text = file.readline().rstrip()
     return pattern, text
 
-def print_occurrences(output):
-    print(*output)
+def print_occurrences(positions):
+    print(' '.join(map(str, positions)))
 
 def get_occurrences(pattern, text):
-    occurrences = []
-    if len(pattern) > len(text):
-        return occurrences
     pattern_hash = hash(pattern)
     text_hash = hash(text[:len(pattern)])
+    positions = []
     for i in range(len(text)-len(pattern)+1):
-        if pattern_hash == text_hash:
-            if text[i:i+len(pattern)] == pattern:
-                occurrences.append(i)
+        if pattern_hash == text_hash and pattern == text[i:i+len(pattern)]:
+            positions.append(i)
         if i < len(text) - len(pattern):
             text_hash = hash(text[i+1:i+len(pattern)+1])
-    return occurrences
+    return positions
 
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
